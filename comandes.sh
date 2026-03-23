73096adc49bf40eb5fffa6ef6d33b66ed03126f8
Alexa <alexandre-tc@outlook.com>
2026-03-23 12:52:38 +0100

    comandes final

--- comandes.sh
+++ comandes.sh
@@ -10,4 +10,5 @@ cd imatges
 touch info_imatge.txt
 echo "La imatge ha estat descarregada de texample.net" >> info_imatge.txt
 wget https://texample.net/files/rotated-triangle.png
-
+mv rotated-triangle.png triangles_tikz.png
+tree -a
